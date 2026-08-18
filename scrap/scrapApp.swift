6b9c64f838aa9103159121cf6987139ebed20648
don <zekkiewashere@icloud.com>
2026-08-18 15:21:43 +0200

    Initial Commit

---
+++ scrap/scrapApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrapApp.swift
+//  scrap
+//
+//  Created by don on 18.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct scrapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ scrapTests/scrapTests.swift
@@ -0,0 +1,18 @@
+//
+//  scrapTests.swift
+//  scrapTests
+//
+//  Created by don on 18.08.2026.
+//
+
+import Testing
+
+struct scrapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
